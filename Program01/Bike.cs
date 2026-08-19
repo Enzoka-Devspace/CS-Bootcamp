602f9abf50d35ada653e193cfc128fefefda1d69
Guilherme <enzplima86@gmail.com>
2026-08-19 15:24:46 -0300

    feat: Criando as Declarações do projeto

---
+++ Program01/Bike.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Program01
+{
+    class Bike
+    {
+        //Sexaclartions
+        public string? Modelo { get; set; }
+        public double Bateria { get; set; }
+        public double PesoCiclista { get; set; }
+
+        public int recargaMes {  get; set; }
+
+
+    }
+}
