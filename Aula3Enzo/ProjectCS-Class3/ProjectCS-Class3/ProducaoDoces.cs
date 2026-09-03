6eed2a7900a2a93ff015432cae0ed2c9324df96b
Enzo Lima <enzplima86@gmail.com>
2026-09-03 15:20:16 -0300

    Add files via upload

---
+++ Aula3Enzo/ProjectCS-Class3/ProjectCS-Class3/ProducaoDoces.cs
@@ -0,0 +1,28 @@
+﻿class ProducaoDoces
+{
+    public string Confeiteiro { get; set; }
+    public double CapacidadeKg { get; set; }
+    public double PorcentagemPronta { get; set; }
+    public double TaxaProducaoPorHora { get; set; }
+
+    public double CalcularKgFaltantes()
+    {
+        double PorcentagemN = (100.0 - PorcentagemPronta) / 100.0;
+        return PorcentagemN;
+    }
+
+    public double CalcularTempoRestanteHoras()
+    {
+        return CalcularKgFaltantes() / TaxaProducaoPorHora;
+    }
+
+    public double CalcularValorFaltante()
+    {
+        const double preco = 45.50;
+        return CalcularKgFaltantes() * preco;
+    }
+    public bool EhProducaoExpressa()
+    {
+        return (TaxaProducaoPorHora >= 20) && (CalcularTempoRestanteHoras() < 1);
+    }
+}
---
+++ Aula3Enzo/ProjectCS-Class3/ProjectCS-Class3/Program.cs
@@ -0,0 +1,40 @@
+﻿while (true)
+{
+    Console.WriteLine("Quer iniciar o sistema? Y/N");
+    string sistema = Console.ReadLine();
+
+    if (sistema == "Y")
+    {
+        InciarSistema();
+
+    }
+    else if (sistema == "N") {
+        Console.WriteLine("\nSistema desligado!");
+        break;
+    }
+
+}
+void AbrirMenu()
+    {
+    ProducaoDoces Pessoa = new ProducaoDoces();
+    Console.WriteLine(
+        "Opção 1 - CalcularKgFaltantes\n" +
+        "Opção 2 - CalcularTempoRestanteHoras\n" +
+        "Opção 3 - CalcularValorFaltante\n" +
+        "Opção 4 - EhProducaoExpressa\n" 
+        );
+    Console.WriteLine("\nEscolha uma opção: ");
+    string opcao = Console.ReadLine();
+    if (opcao == "1")
+    {
+        Console.WriteLine("\nCALCULADORA DE KG FALTANTES\nDIGITE A PORCENTAGEM PRONTA:\n ");
+        string Porcentagem = Console.ReadLine();
+        Pessoa.PorcentagemPronta = Convert.ToDouble(Porcentagem);
+        Console.WriteLine($"\nResultado: {Pessoa.CalcularKgFaltantes()}");
+    }
+} 
+
+void InciarSistema()
+{
+    AbrirMenu();
+}
